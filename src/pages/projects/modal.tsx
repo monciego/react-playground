1fe66ae370bf4081ee85eca9ad70b18191410578
Jericho <jerichobantiquete@gmail.com>
2023-09-11 20:15:31 +0800

    style: fix styles in modal project

--- src/pages/projects/modal.tsx
+++ src/pages/projects/modal.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
+import Button from '../../components/button';
 
 export default function Modal() {
   const [showModal, setShowModal] = React.useState(false);
   return (
     <>
-      <button
-        className='bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-        type='button'
-        onClick={() => setShowModal(true)}
-      >
-        Open regular modal
-      </button>
+      <Button onClick={() => setShowModal(true)}>Open Modal</Button>
       {showModal ? (
         <>
           <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0  outline-none focus:outline-none'>
@@ -18,12 +13,14 @@ export default function Modal() {
               onClick={() => setShowModal(false)}
               className='opacity-50 fixed inset-0 z-[40] bg-black'
             ></div>
-            <div className='relative w-auto my-6 mx-auto max-w-3xl'>
+            <div className='relative w-auto my-6 mx-4 max-w-3xl'>
               {/*content*/}
               <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full z-[50] bg-white outline-none focus:outline-none'>
                 {/*header*/}
                 <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
-                  <h3 className='text-3xl font-semibold'>Modal Title</h3>
+                  <h3 className='text-3xl text-slate-950 font-semibold'>
+                    Modal Title
+                  </h3>
                   <button
                     className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
                     onClick={() => setShowModal(false)}
@@ -34,7 +31,7 @@ export default function Modal() {
                   </button>
                 </div>
                 {/*body*/}
-                <div className='relative p-6 flex-auto'>
+                <div className='relative px-6 flex-auto'>
                   <p className='my-4 text-slate-500 text-lg leading-relaxed'>
                     I always felt like I could do anything. That’s the main
                     thing people are controlled by! Thoughts- their perception
@@ -52,13 +49,9 @@ export default function Modal() {
                   >
                     Close
                   </button>
-                  <button
-                    className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                    type='button'
-                    onClick={() => setShowModal(false)}
-                  >
+                  <Button onClick={() => setShowModal(false)}>
                     Save Changes
-                  </button>
+                  </Button>
                 </div>
               </div>
             </div>
